8275f041651e8c6e1a4f0bd0e7fcb303157c8f6c
WizzyFX <ugurcank@gmail.com>
2019-08-04 20:39:06 -0700

    Initial Commit

--- NerdeRegion-Chrome/src/inject/inject.js
+++ NerdeRegion-Chrome/src/inject/inject.js
@@ -5,18 +5,46 @@
  */
 
 const NerdeFocus = (function () {
+	const regions = [
+		'[aria-live="polite"]',
+		'[aria-live="assertive"]',
+		'alert',
+		'log',
+		'status',
+		'[role="alert"]',
+		'[role="log"]',
+		'[role="status"]'
+	];
 
-    const sendObjectToDevTools = (message) => {
-        chrome.extension.sendMessage(message);
-    };
+	const sendObjectToDevTools = (message) => {
+		chrome.extension.sendMessage(message);
+	};
 
-    const initialize = () => {
+	const findRegions = () => {
+		return document.querySelectorAll(regions.join(','));
+	};
 
-    };
+	const callback = (mutationsList)=>{
+		for(let mutation of mutationsList) {
+			if (mutation.type === 'childList' && mutation.addedNodes !== undefined) {
+				for(let addedNode of mutation.addedNodes) {
+					if(addedNode.attributes !== undefined && 'aria-live' in addedNode.attributes){
+						console.log(findRegions());
+					}
+				}
+			}
+		}
+	};
 
-    if (document.readyState != 'loading'){
-        initialize();
-    } else {
-        document.addEventListener('DOMContentLoaded', initialize);
-    }
+	const initialize = () => {
+		console.log(findRegions());
+		const observer = new MutationObserver(callback);
+		observer.observe(document.body, { attributes: true, childList: true, subtree: true });
+	};
+
+	if (document.readyState != 'loading'){
+		initialize();
+	} else {
+		document.addEventListener('DOMContentLoaded', initialize);
+	}
 })();
--- NerdeRegion-Chrome/src/service/background.js
+++ NerdeRegion-Chrome/src/service/background.js
@@ -1,24 +1,24 @@
 chrome.runtime.onConnect.addListener(function (port) {
-    var extensionListener = function (message, sender) {
-        if (message.tabId && message.content) {
-            if (message.action === 'code') {
-                chrome.tabs.executeScript(message.tabId, {code: message.content});
-            } else if (message.action === 'script') {
-                chrome.tabs.executeScript(message.tabId, {file: message.content});
-            } else {
-                chrome.tabs.sendMessage(message.tabId, message);
-            }
-        } else {
-            port.postMessage({content: message, sender: sender});
-        }
-    };
-    chrome.extension.onMessage.addListener(extensionListener);
+	var extensionListener = function (message, sender) {
+		if (message.tabId && message.content) {
+			if (message.action === 'code') {
+				chrome.tabs.executeScript(message.tabId, {code: message.content});
+			} else if (message.action === 'script') {
+				chrome.tabs.executeScript(message.tabId, {file: message.content});
+			} else {
+				chrome.tabs.sendMessage(message.tabId, message);
+			}
+		} else {
+			port.postMessage({content: message, sender: sender});
+		}
+	};
+	chrome.extension.onMessage.addListener(extensionListener);
 
-    port.onDisconnect.addListener(function (port) {
-        chrome.extension.onMessage.removeListener(extensionListener);
-    });
+	port.onDisconnect.addListener(function (port) {
+		chrome.extension.onMessage.removeListener(extensionListener);
+	});
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender) {
-    return true;
+	return true;
 });
