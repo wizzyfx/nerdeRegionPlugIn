9ee93d7b8622c99ce7c6d06d2b072218dd5cbf63
WizzyFX <ugurcank@gmail.com>
2019-08-04 22:51:11 -0700

    Initial Commit

--- NerdeRegion-Chrome/src/inject/inject.js
+++ NerdeRegion-Chrome/src/inject/inject.js
@@ -5,12 +5,10 @@
  */
 
 const NerdeFocus = (function () {
+
 	const regions = [
 		'[aria-live="polite"]',
 		'[aria-live="assertive"]',
-		'alert',
-		'log',
-		'status',
 		'[role="alert"]',
 		'[role="log"]',
 		'[role="status"]'
@@ -21,28 +19,55 @@ const NerdeFocus = (function () {
 	};
 
 	const findRegions = () => {
-		return document.querySelectorAll(regions.join(','));
+		document.querySelectorAll(regions.join(',')).forEach((element) => {
+			if(element.nerderegion === undefined) {
+				element.nerderegion = true;
+				element.dataset.nerderegion = 'init';
+				console.log(element);
+			}
+		});
+	};
+
+	const checkRegion = (mutation) => {
+		if (mutation.type === 'childList' && mutation.addedNodes !== undefined) {
+			for(let node of mutation.addedNodes) {
+				if(node.attributes !== undefined && 'aria-live' in node.attributes && ['polite', 'assertive'].includes(node.attributes['aria-live'].nodeValue.toLowerCase())) {
+					return true;
+				}
+			}
+		}
+		return false;
+	};
+
+	const checkRole = (mutation) => {
+		if (mutation.type === 'childList' && mutation.addedNodes !== undefined) {
+			for(let node of mutation.addedNodes) {
+				if(node.attributes !== undefined && 'role' in node.attributes && ['alert', 'log', 'status'].includes(node.attributes['role'].nodeValue.toLowerCase())) {
+					if('aria-live' in node.attributes && node.attributes['aria-live'].nodeValue.toLowerCase() === 'off') {
+						return false;
+					}
+					return true;
+				}
+			}
+		}
+		return false;
 	};
 
 	const callback = (mutationsList)=>{
 		for(let mutation of mutationsList) {
-			if (mutation.type === 'childList' && mutation.addedNodes !== undefined) {
-				for(let addedNode of mutation.addedNodes) {
-					if(addedNode.attributes !== undefined && 'aria-live' in addedNode.attributes){
-						console.log(findRegions());
-					}
-				}
+			if(checkRegion(mutation) || checkRole(mutation)) {
+				findRegions();
 			}
 		}
 	};
 
 	const initialize = () => {
-		console.log(findRegions());
+		findRegions();
 		const observer = new MutationObserver(callback);
 		observer.observe(document.body, { attributes: true, childList: true, subtree: true });
 	};
 
-	if (document.readyState != 'loading'){
+	if (document.readyState != 'loading') {
 		initialize();
 	} else {
 		document.addEventListener('DOMContentLoaded', initialize);
