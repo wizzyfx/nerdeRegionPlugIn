84ecc9544b582ad68e92266b18e2426c8fa9ac6d
Ugi Kutluoglu <ugi@scribd.com>
2019-08-27 13:18:16 -0700

    Initial Commit

--- NerdeRegion-Chrome/src/panel/panel.js
+++ NerdeRegion-Chrome/src/panel/panel.js
@@ -1,41 +1,51 @@
-(function createChannel () {
-	//Create a port with service page for continuous message communication
-	const port = chrome.extension.connect({
-		name: "nerdeRegionChromeExtension"
-	});
+(function createChannel() {
+  //Create a port with service page for continuous message communication
+  const port = chrome.extension.connect({
+    name: "nerdeRegionChromeExtension",
+  });
 
-	// Listen to messages from the service page
-	port.onMessage.addListener(function (message) {
-		if (message.sender.tab.id == chrome.devtools.inspectedWindow.tabId) {
-			processIncoming (message);
-		}
-	});
-}());
+  // Listen to messages from the service page
+  port.onMessage.addListener(function(message) {
+    if (message.sender.tab.id == chrome.devtools.inspectedWindow.tabId) {
+      processIncoming(message);
+    }
+  });
+})();
 
 Rainbow.defer = true;
-const eventsContainer = document.querySelector('#events');
-const eventsList = document.querySelector('#events ol');
-const regionsContainer = document.querySelector('#regions');
+const eventsContainer = document.querySelector("#events");
+const eventsList = document.querySelector("#events ol");
+const regionsContainer = document.querySelector("#regions");
 
-function padZero (number, length) {
-	return ('0'.repeat(length) + number).slice(-length);
+function padZero(number, length) {
+  return ("0".repeat(length) + number).slice(-length);
 }
 
-function processIncoming (message) {
-	const regionId = message.content.data[0];
-	const regionCSS = message.content.data[1];
-	const regionAccName = message.content.data[3];
-	//let regionHTMLContent = $('<div />').text(message.content.data[2]).html();
-	const regionInFrame = message.content.framed;
-	const isScrollAble = Math.abs((eventsContainer.scrollTop + eventsContainer.offsetHeight) - eventsContainer.scrollHeight) < 10;
+function processIncoming(message) {
+  const regionId = message.content.data[0];
+  const regionCSS = message.content.data[1];
+  const regionAccName = message.content.data[3];
+  //let regionHTMLContent = $('<div />').text(message.content.data[2]).html();
+  const regionInFrame = message.content.framed;
+  const isScrollAble =
+    Math.abs(
+      eventsContainer.scrollTop +
+        eventsContainer.offsetHeight -
+        eventsContainer.scrollHeight
+    ) < 10;
 
-	const currentTime = new Date();
-	const timestamp = `${padZero(currentTime.getHours(), 2)}:${padZero(currentTime.getMinutes(), 2)}:${padZero(currentTime.getSeconds(), 2)}:${padZero(currentTime.getMilliseconds(), 3)}`;
+  const currentTime = new Date();
+  const timestamp = `${padZero(currentTime.getHours(), 2)}:${padZero(
+    currentTime.getMinutes(),
+    2
+  )}:${padZero(currentTime.getSeconds(), 2)}:${padZero(
+    currentTime.getMilliseconds(),
+    3
+  )}`;
 
-
-	Rainbow.color(message.content.data[2], 'html', function(highlightedCode) {
-		$(eventsList).append(
-			`<li class="region-${regionId}">
+  Rainbow.color(message.content.data[2], "html", function(highlightedCode) {
+    $(eventsList).append(
+      `<li class="region-${regionId}">
 			<span class="role meta"><strong>Role:</strong> Alert</span>
 			<span class="type meta"><strong>Politeness:</strong> Assertive</span>
 			<span class="atomic meta"><strong>Atomic:</strong> True</span>
@@ -45,33 +55,41 @@ function processIncoming (message) {
 			<div class="content accname">${regionAccName}</div>
 			<div class="content html" data-language="html"><pre>${highlightedCode}</pre></div>
 			<div class="time">${timestamp}</div>
-        </li>`);
-		if(isScrollAble) {
-			eventsContainer.scrollTop = eventsContainer.scrollHeight;
-		}
-	});
+        </li>`
+    );
+    if (isScrollAble) {
+      eventsContainer.scrollTop = eventsContainer.scrollHeight;
+    }
+  });
 
-	if(!regionsContainer.querySelector(`.region-${regionId}`)) {
-		$(regionsContainer).append(
-			`<li role="none" class="region region-${regionId}">
+  if (!regionsContainer.querySelector(`.region-${regionId}`)) {
+    $(regionsContainer).append(
+      `<li role="none" class="region region-${regionId}">
 				<button role="tab" aria-selected="false" aria-controls="events" class="tab">
 					<em class="id">${regionId}</em>${regionCSS}
 				</button>
-			</li>`);
-	}
+			</li>`
+    );
+  }
 }
 
-$('.check').on('click', function () {
-	if ($(this).hasClass('on')) {
-		$(this).removeClass('on').attr('aria-label', 'Turn On Focus Indicator');
-	} else {
-		$(this).addClass('on').attr('aria-label', 'Turn Off Focus Indicator');
-	}
+$(".check").on("click", function() {
+  if ($(this).hasClass("on")) {
+    $(this)
+      .removeClass("on")
+      .attr("aria-label", "Turn On Focus Indicator");
+  } else {
+    $(this)
+      .addClass("on")
+      .attr("aria-label", "Turn Off Focus Indicator");
+  }
 });
 
-$('#clearButton').on('click', function () {
-	$(eventsList).empty();
-	$(regionsContainer).html('<li role="none"><button role="tab" aria-selected="false" aria-controls="events" class="tab all active">All Regions</button></li>');
+$("#clearButton").on("click", function() {
+  $(eventsList).empty();
+  $(regionsContainer).html(
+    '<li role="none"><button role="tab" aria-selected="false" aria-controls="events" class="tab all active">All Regions</button></li>'
+  );
 });
 
-$('body').addClass(chrome.devtools.panels.themeName);
+$("body").addClass(chrome.devtools.panels.themeName);
